8f145710e1827d8eaf0c71632196d041d68b1125
Jean <jeanncavalcante@gmail.com>
2022-08-04 17:47:23 -0300

    ellos

---
+++ php/adicionar_action.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'config.php';
+
+$name = filter_input(INPUT_POST, 'name');
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$tel = filter_input(INPUT_POST, 'tel');
+$message = filter_input(INPUT_POST, 'message');
+
+if( $name && $email && $tel && $message ){
+
+  $sql = $pdo->prepare('SELECT * FROM usuarios WHERE email = :email');
+  $sql->bindValue(':email', $email);
+  $sql->execute();
+
+  if( $sql->rowCount() == 0 ){
+  $sql = $pdo->prepare("INSERT INTO users (nome, email, telefone, comentario_cliente) 
+  VALUES (:name, :email, :tel, :message)");
+  $sql->bindValue(':name', $name);
+  $sql->bindValue(':email', $email);
+  $sql->bindValue(':tel', $tel);
+  $sql->bindValue(':message', $message);
+  $sql->execute();
+
+  header('Location: ../sucess.html');
+  exit;
+  }
+}
---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'heroku_7b495a6f796de16';
+$db_host = 'us-cdbr-east-06.cleardb.net';
+$db_user = 'b436097193a82a';
+$db_pass = 'bb5ecdb3';
+
+$pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_pass);
+
