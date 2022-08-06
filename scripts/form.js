6e946b370ff200a9f202dff9de8b2ecc377a2005
Jean <jeanncavalcante@gmail.com>
2022-08-06 16:53:45 -0300

    refature form

--- scripts/form.js
+++ scripts/form.js
@@ -1,10 +1,12 @@
 var firstName = document.getElementById("name");
 var email = document.getElementById("email");
 var tel = document.getElementById("tel");
+var message = document.getElementById("message");
 
 var nameError = document.getElementById("errorName");
 var emailError = document.getElementById("errorEmail");
 var telError = document.getElementById("errorTel");
+var messageError = document.getElementById("errorMessage");
 
 function validateForm() {
   var valid = true;
@@ -14,17 +16,26 @@ function validateForm() {
   } else {
     nameError.innerHTML = "";
   }
-  if (email.value == "") {
-    emailError.innerHTML = "*Digite um email valido";
+  if (email.value == "" || tel.value == "" || message.value == "") {
+    emailError.innerHTML = "*Campo obrigatório";
     valid = false;
   } else {
     emailError.innerHTML = "";
   }
+
   if (tel.value == "") {
-    telError.innerHTML = "*Digite um telefone valido";
+    telError.innerHTML = "*Campo obrigatório";
     valid = false;
   } else {
     telError.innerHTML = "";
+  }
+
+  if (message.value == "") {
+    messageError.innerHTML = "*Campo obrigatório";
+    valid = false;
+    console.log("mensagem:", message.value);
+  } else {
+    messageError.innerHTML = "";
   }
   return valid;
 }
